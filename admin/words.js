3ce4f5c676c329dc650cef1aa8927c19740d5f39
swissglider <npm@mailschweiz.com>
2020-10-11 11:17:49 +0200

    iobroker init
    ..

---
+++ admin/words.js
@@ -0,0 +1,53 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'swissglider1adapter adapter settings': {
+		en: 'Adapter settings for swissglider1adapter',
+		de: 'Adaptereinstellungen für swissglider1adapter',
+		ru: 'Настройки адаптера для swissglider1adapter',
+		pt: 'Configurações do adaptador para swissglider1adapter',
+		nl: 'Adapterinstellingen voor swissglider1adapter',
+		fr: "Paramètres d'adaptateur pour swissglider1adapter",
+		it: "Impostazioni dell'adattatore per swissglider1adapter",
+		es: 'Ajustes del adaptador para swissglider1adapter',
+		pl: 'Ustawienia adaptera dla swissglider1adapter',
+		'zh-cn': 'swissglider1adapter的适配器设置',
+	},
+	'loading...': {
+		en: 'loading...',
+		de: 'Wird geladen...',
+		ru: 'загрузка ...',
+		pt: 'Carregando...',
+		nl: 'bezig met laden...',
+		fr: 'chargement...',
+		it: 'Caricamento in corso...',
+		es: 'cargando...',
+		pl: 'Ładuję...',
+		'zh-cn': '载入中...',
+	},
+	option1: {
+		en: 'option1',
+		de: 'Option 1',
+		ru: 'Опция 1',
+		pt: 'Opção 1',
+		nl: 'Optie 1',
+		fr: 'Option 1',
+		it: 'opzione 1',
+		es: 'Opción 1',
+		pl: 'opcja 1',
+		'zh-cn': '选项1',
+	},
+	option2: {
+		en: 'option2',
+		de: 'Option 2',
+		ru: 'option2',
+		pt: 'opção 2',
+		nl: 'Optie 2',
+		fr: 'Option 2',
+		it: 'opzione 2',
+		es: 'opcion 2',
+		pl: 'Opcja 2',
+		'zh-cn': '选项2',
+	},
+};
